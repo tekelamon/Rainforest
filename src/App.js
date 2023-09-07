fe38a2c2de4f725ed69c14b74e9edc114a896447
Teke Byas <tekelamon01@gmail.com>
2023-09-07 14:36:12 -0700

    begin new component to show products

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Products from './components/Products';
 
 function App() {
   return (
     <div className="App">
+      <Products />
     </div>
   );
 }
---
+++ src/components/Products.js
@@ -0,0 +1,11 @@
+// a place to display all products returned from api in card elements
+function Products() {
+    // get products from api
+    // map to card elements
+    // return jsx with cards filled in
+    return (
+        <p>Test</p>
+    );
+}
+
+export default Products;
