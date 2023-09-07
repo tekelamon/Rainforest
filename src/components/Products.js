ecc1eb88b18d151cc4d32346267a584581d90469
Teke Byas <tekelamon01@gmail.com>
2023-09-07 14:54:43 -0700

    connected to api, returning titles for display

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,10 +1,29 @@
+import { useState, useEffect } from "react";
+
 // a place to display all products returned from api in card elements
 function Products() {
+    const [products, setProducts] = useState([]);
+
     // get products from api
-    // map to card elements
-    // return jsx with cards filled in
+    useEffect(()=>{
+        const getAllProducts = async () => {
+            fetch('https://fakestoreapi.com/products')
+                .then( res => res.json() )
+                .then( json => setProducts( json ) )
+        };
+        getAllProducts();
+    },[]);
+
     return (
-        <p>Test</p>
+        <div id="all-products">
+            <ul>
+            {
+                products.map( product => {
+                    return <li key={product.id}>{product.title}</li>
+                })
+            }
+            </ul>
+        </div>
     );
 }
 
