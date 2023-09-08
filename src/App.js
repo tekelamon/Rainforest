3d9b2a04c2f9b8230091a2047eb9e8a59ce62ab3
Teke Byas <tekelamon01@gmail.com>
2023-09-07 18:13:45 -0700

    added browser router to navigate site

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import './App.css';
-import Products from './components/Products';
+
+// pages
+import Navigation from './components/Navigation';   // navigation across top of screen
+import Products from './components/Products';       // main browsing page of products
+import Product from './components/Product';         // individual product details
+import Cart from './components/Cart';               // entire cart
+
+import Login from './components/Login';             // login page
+import Signup from './components/Signup';           // signup page
+
 
 function App() {
   return (
     <div className="App">
-      <Products />
+      <BrowserRouter>
+        <Routes>
+          {/* these routes will share the navigation at top of screen */}
+          <Route element={<Navigation />} path='/' >
+            <Route index element={<Products />} />
+            <Route element={<Product />} path='/product' />
+            <Route element={<Cart />} path='/cart' />
+          </Route>
+
+          <Route element={<Login />} path='/login' />
+          <Route element={<Signup />} path='/signup' />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
