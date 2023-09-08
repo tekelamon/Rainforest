a21e9abd5d4acf1f342f76738431f61da2193117
Teke Byas <tekelamon01@gmail.com>
2023-09-07 18:12:50 -0700

    connected template components with router

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,17 @@
+import { Link, Outlet } from "react-router-dom";
+
 function Navigation() {
-    return <p>Navigation</p>;
+    return ( <>
+        <nav>
+            <Link to='/' >Browse Products</Link>
+            <Link to='./cart' >View Cart</Link>
+
+            <Link to='./login' >Login</Link>
+            <Link to='./signup' >Signup</Link>
+        </nav>
+
+        <Outlet />
+    </>);
 }
 
 export default Navigation;
