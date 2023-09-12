117dc54d27a9fc9ba1631e2706c4f6da05ebccbe
Teke Byas <tekelamon01@gmail.com>
2023-09-11 19:21:53 -0700

    added base content for product details view without styling

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,10 +1,30 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { getProductById } from "./api-services";
+import { useState, useEffect } from "react";
 
 const ProductDetails = () => {
-    // gets the id of the individual product from the link passed in
-    const product = useParams();
+    const [product, setProduct] = useState({});
 
-    return <p>Product Details for {product.id} </p>;
+    // get id of the individual product from the link passed in
+    const passedProduct = useParams();
+
+    // get data for product to display
+    useEffect(()=>{
+        const getData = async () => {
+            const response = await getProductById(passedProduct.id);
+            setProduct( response );
+        };
+        getData();
+    },[]);
+
+    return <div>
+        <img src={product.image} className="product-details-image"/>
+        <h2 className="product-details-title">{product.title}</h2>
+        <p className="product-details-price">{product.price}</p>
+        {/* TODO make button to add to cart when cart is functional */}
+        <p>Add to cart</p>
+        <Link to="/">Go Home</Link>
+    </div>
 }
 
 export default ProductDetails;
