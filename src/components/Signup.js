e3b70f05cce7394b43557b1899d4f91525c46b3b
Teke Byas <tekelamon01@gmail.com>
2023-09-14 19:40:37 -0700

    begin singup form

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,31 @@
 function Signup() {
-    return <p>Signup</p>;
+    // connect to api to authenticate responses
+    // although actual accounts will not be made
+    return (
+        <div id="signupForm-container">
+            <form id="signupForm">
+                <label
+                    htmlFor="username"
+                    className="signupFormText"
+                >Username: </label>
+                <input
+                    className="signupFormInput"
+                    type="text"
+                    id="username"
+                />
+                <label
+                    htmlFor="password"
+                    className="signupFormText"
+                >Password: </label>
+                <input
+                    className="signupFormInput"
+                    type="text"
+                    id="password"
+                />
+                <input id="signupFormSubmit" type="submit" value="Submit" />
+            </form>
+        </div>
+    )
 }
 
 export default Signup;
