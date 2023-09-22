f437be0e3e44ba87b6b75fa9bbf78a139c77ab9e
Teke Byas <tekelamon01@gmail.com>
2023-09-21 18:54:17 -0700

    added basic login service

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,50 @@
+import { loginUser } from "./api-services";
+import { useEffect, useState } from "react";
+
 function Login() {
-    return <p>Login</p>;
+    const [inputs, setInputs] = useState(null);
+
+    useEffect(()=>{
+        const attemptLogin = async () => {
+            const response = await loginUser( inputs );
+            console.log( response );
+        };
+        // only attempt login after inputs are received
+        if( inputs ) { attemptLogin(); }
+    // updates when new input is entered
+    },[inputs]);
+
+    const handleSubmit = event => {
+        event.preventDefault();
+
+        // get data from form
+        const data = new FormData( event.target );
+
+        // update received inputs
+        const newLoginAttempt = {
+            username:data.get("username"),
+            password:data.get("password")
+        };
+        console.log( {newLoginAttempt} );
+        setInputs( newLoginAttempt );
+
+        // update user
+        console.log("Hey I'm trying here!");
+    };
+
+    return (
+        <div id="loginFormContainer">
+            <form id="loginForm" onSubmit={event => handleSubmit(event) } >
+                <label className="loginFormText" htmlFor="username" >Username: </label>
+                <input id="username" name="username" className="loginFormInput" />
+
+                <label className="loginFormText" htmlFor="password" >Password: </label>
+                <input id="password" name="password" className="loginFormInput" />
+
+                <input type="submit" id="loginFormSubmit" value="Login" />
+            </form>
+        </div>
+    )
 }
 
 export default Login;
--- src/components/api-services.js
+++ src/components/api-services.js
@@ -4,6 +4,8 @@ const BASE_API = 'https://fakestoreapi.com';
 const PRODUCTS = `${BASE_API}/products`;
 const ALL_CATEGORIES = `${PRODUCTS}/categories`;
 const CATEGORIES = `${PRODUCTS}/category`;
+const USERS = `${BASE_API}/users`;
+const LOGIN = `${BASE_API}/auth/login`;
 
 const getAllProducts = async () => {
     try {
@@ -67,9 +69,32 @@ const createUser = async (user) => {
                 }
             )
         };
-        const response = await fetch('https://fakestoreapi.com/users', message );
+        const response = await fetch(`${USERS}`, message );
         const createUserResponse = await response.json();
         return createUserResponse;
+    } catch (err) {
+        console.error( err );
+    }
+};
+
+const loginUser = async (input) => {
+    try {
+        const message = {
+            method:"POST",
+            body:JSON.stringify(
+                {
+                    username:`${input.username}`,
+                    password:`${input.password}`
+                }
+            ),
+            headers:{
+                "Content-Type":"application/json"
+            },
+            redirect:"follow"
+        };
+        const response = await fetch(`${LOGIN}`, message );
+        const result = await response.json();
+        return result;
     } catch (err) {
         console.error( err );
     }
@@ -80,5 +105,6 @@ export {
     getProductById,
     getAllCategories,
     getProductsByCategory,
-    createUser
+    createUser,
+    loginUser
 }
